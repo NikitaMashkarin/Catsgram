1ef519420bd80cff63dea72835b78ece5ca8045d
NikitaMashkarin <mashkarinnikita@yandex.ru>
2024-12-18 03:21:36 +0300

    Исправить ошибку в ImageFileException

--- src/main/java/ru/yandex/practicum/catsgram/exception/ImageFileException.java
+++ src/main/java/ru/yandex/practicum/catsgram/exception/ImageFileException.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.catsgram.exception;
 
 public class ImageFileException extends RuntimeException {
     public ImageFileException(String message, Exception e) {
-        super(message);
+        super(message, e);
     }
 
     public ImageFileException(String message) {
